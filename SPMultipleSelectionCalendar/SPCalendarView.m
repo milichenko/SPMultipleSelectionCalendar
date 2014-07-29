ce82fe1f3064a8426549d731dce07e17ee0503cf
milichenko <milichenko@maximagroup.com>
2014-07-29 19:23:45 +0300

    month selection

--- SPMultipleSelectionCalendar/SPCalendarView.m
+++ SPMultipleSelectionCalendar/SPCalendarView.m
@@ -10,6 +10,16 @@
 
 #define CONTROLS_WIDTH 320 / 7.0f
 
+@interface SPCalendarView ()
+
+@property (strong, nonatomic) NSDate *selectedDate;
+@property (strong, nonatomic) NSDateFormatter *dateFormatter;
+@property (strong, nonatomic) UIButton *previousMonthButton;
+@property (strong, nonatomic) UIButton *nextMonthButton;
+@property (strong, nonatomic) UILabel *dateLabel;
+
+@end
+
 @implementation SPCalendarView
 
 - (id)initWithFrame:(CGRect)frame
@@ -18,6 +28,10 @@
     
     if (self)
     {
+        self.selectedDate = [NSDate date];
+        self.dateFormatter = [[NSDateFormatter alloc] init];
+        [self.dateFormatter setDateFormat:@"MMMM yyyy"];
+        
         self.backgroundColor = [UIColor whiteColor];
         
         UIView *firstLineView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 42.5f, self.frame.size.width, 0.5f)];
@@ -28,22 +42,59 @@
         secondLineView.backgroundColor = [UIColor colorWithWhite:238.0f / 255.0f alpha:1.0f];
         [self addSubview:secondLineView];
         
-        UIButton *previousMonthButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        previousMonthButton.frame = CGRectMake(0.0f, 0.0f, CONTROLS_WIDTH, 42.5f);
-        [previousMonthButton setTitleColor:[UIColor colorWithRed:99 / 255.0f green:87 / 255.0f blue:207 / 255.0f alpha:1.0f] forState:UIControlStateNormal];
-        [previousMonthButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
-        [previousMonthButton setTitle:@"<" forState:UIControlStateNormal];
-        [self addSubview:previousMonthButton];
+        self.previousMonthButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        self.previousMonthButton.frame = CGRectMake(0.0f, 0.0f, CONTROLS_WIDTH, 42.5f);
+        [self.previousMonthButton setTitleColor:[UIColor colorWithRed:99 / 255.0f green:87 / 255.0f blue:207 / 255.0f alpha:1.0f] forState:UIControlStateNormal];
+        [self.previousMonthButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
+        [self.previousMonthButton setTitle:@"<" forState:UIControlStateNormal];
+        [self.previousMonthButton addTarget:self action:@selector(previousMonthButtonHandler) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:self.previousMonthButton];
         
-        UIButton *nextMonthButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        nextMonthButton.frame = CGRectMake(self.frame.size.width - CONTROLS_WIDTH, 0.0f, CONTROLS_WIDTH, 42.5f);
-        [nextMonthButton setTitleColor:[UIColor colorWithRed:99 / 255.0f green:87 / 255.0f blue:207 / 255.0f alpha:1.0f] forState:UIControlStateNormal];
-        [nextMonthButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
-        [nextMonthButton setTitle:@">" forState:UIControlStateNormal];
-        [self addSubview:nextMonthButton];
+        self.nextMonthButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        self.nextMonthButton.frame = CGRectMake(self.frame.size.width - CONTROLS_WIDTH, 0.0f, CONTROLS_WIDTH, 42.5f);
+        [self.nextMonthButton setTitleColor:[UIColor colorWithRed:99 / 255.0f green:87 / 255.0f blue:207 / 255.0f alpha:1.0f] forState:UIControlStateNormal];
+        [self.nextMonthButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
+        [self.nextMonthButton setTitle:@">" forState:UIControlStateNormal];
+        [self.nextMonthButton addTarget:self action:@selector(nextMonthButtonHandler) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:self.nextMonthButton];
+        
+        self.dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.previousMonthButton.frame.origin.x + self.previousMonthButton.frame.size.width,
+                                                                   self.previousMonthButton.frame.origin.y,
+                                                                   self.frame.size.width - (self.previousMonthButton.frame.origin.x + self.previousMonthButton.frame.size.width) - self.nextMonthButton.frame.size.width,
+                                                                   self.previousMonthButton.frame.size.height)];
+        self.dateLabel.textColor = [UIColor colorWithRed:99 / 255.0f green:87 / 255.0f blue:207 / 255.0f alpha:1.0f];
+        self.dateLabel.textAlignment = NSTextAlignmentCenter;
+        self.dateLabel.text = [self.dateFormatter stringFromDate:self.selectedDate];
+        [self addSubview:self.dateLabel];
     }
     
     return self;
+}
+
+#pragma mark - Button handlers
+
+- (void)addMonthCountToDateLabel:(NSInteger)monthCount
+{
+    NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+    [dateComponents setMonth:monthCount];
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    
+    self.selectedDate = [calendar dateByAddingComponents:dateComponents toDate:self.selectedDate options:0];
+    
+    self.dateLabel.text = [self.dateFormatter stringFromDate:self.selectedDate];
+}
+
+#pragma mark - Button handlers
+
+- (void)previousMonthButtonHandler
+{
+    [self addMonthCountToDateLabel:-1];
+}
+
+- (void)nextMonthButtonHandler
+{
+    [self addMonthCountToDateLabel:1];
 }
 
 @end
